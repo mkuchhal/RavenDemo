e310141c5a6fd0f3c02e4a09d7910733a13b4143
Mrinal Kuchhal <mk@breakingwavesoftware.com>
2012-01-30 12:51:07 +0530

    asdasd

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,8 @@ class Message < ActiveRecord::Base
 
 after_save do |message|
     
-    puts "#########TEST######## " + ServerFactory::SURFBOARD.send_request("?req=addMarker&position=#{message.latitude},#{message.longitude}")
+    puts "#########TEST######## " 
+    puts  ServerFactory::SURFBOARD.send_request("?req=addMarker&position=#{message.latitude},#{message.longitude}")
+    puts "#####TEST####"
   end
 end
