be25df6a025e659ed5775f83337181d0a494c640
Mrinal Kuchhal <mk@breakingwavesoftware.com>
2012-01-30 16:44:22 +0530

    jan 30

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,12 @@
 class Message < ActiveRecord::Base
 
+validates :Messagetext, :presence =>true
+validates :Latitude, :presence =>true
+validates :Longitude, :presence =>true
+validates :Sendername, :presence =>true
+validates :Recepientname, :presence =>true
+
+
 after_save do |message|
     
     puts "#########TEST######## " 
