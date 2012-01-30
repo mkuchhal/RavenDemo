12f9684a5a665856acebb77ec1fda53b613e8ef6
Mrinal Kuchhal <mk@breakingwavesoftware.com>
2012-01-30 12:42:01 +0530

    checking sb response

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,6 @@ class Message < ActiveRecord::Base
 
 after_save do |message|
     
-    ServerFactory::SURFBOARD.send_request("?req=addMarker&position=#{message.latitude},#{message.longitude}")
+    puts ServerFactory::SURFBOARD.send_request("?req=addMarker&position=#{message.latitude},#{message.longitude}")
   end
 end
